048d915c3ee8336908245791eb08af8dfdca825a
ysjprojects <yshijie1999@gmail.com>
2021-06-30 23:23:58 +0800

    changed ico

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+BASE = "https://murmuring-peak-27971.herokuapp.com/"
+
+response = requests.get(BASE + 'all')
+print(response.json())
+response = requests.get(BASE + 'fields')
+print(response.json())
+response = requests.get(BASE + 'categories')
+print(response.json())
+'''
+api.add_resource(All, "/all")
+api.add_resource(Fields, "/fields")
+api.add_resource(DataByField, "/field/<string:field>")
+api.add_resource(Categories, "/categories")
+api.add_resource(DataByCategory, "/category/<string:category>")
+'''
